18d2f3709be6b2d0e5624165834d6ebe00ad7cb8
makskrevskyi <91610843+makskrevskyi@users.noreply.github.com>
2021-11-14 03:19:10 +0200

    Add files via upload

---
+++ Lesson8/stoli.js
@@ -0,0 +1,85 @@
+const num = prompt('Введіть число (від 1 до 100)');
+if (num == 1) {
+    console.log(num + ' стіл')
+} else if (num >100) {
+    console.log(num + ' - не підходящий номер')
+} else if (num <0) {
+    console.log(num + ' - не підходящий номер')
+} else if (num == 2) {
+    console.log(num + ' стoли')
+} else if (num == 3) {
+    console.log(num + ' стoли')
+} else if (num == 4) {
+    console.log(num + ' стoли')
+} else if (num ==21) {
+    console.log(num + ' стіл')
+} else if (num ==31) {
+    console.log(num + ' стіл')
+} else if (num ==41) {
+    console.log(num + ' стіл')
+} else if (num ==51) {
+    console.log(num + ' стіл')
+} else if (num ==61) {
+    console.log(num + ' стіл')
+} else if (num ==71) {
+    console.log(num + ' стіл')
+} else if (num ==81) {
+    console.log(num + ' стіл')
+} else if (num ==91) {
+    console.log(num + ' стіл')
+}
+ else if (num ==22) {
+    console.log(num + ' столи')
+} else if (num ==23) {
+    console.log(num + ' столи')
+} else if (num ==24) {
+    console.log(num + ' столи')
+} else if (num ==32) {
+    console.log(num + ' столи')
+} else if (num ==33) {
+    console.log(num + ' столи')
+} else if (num ==34) {
+    console.log(num + ' столи')
+} else if (num ==42) {
+    console.log(num + ' столи')
+} else if (num ==43) {
+    console.log(num + ' столи')
+} else if (num ==44) {
+    console.log(num + ' столи')
+}  else if (num ==52) {
+    console.log(num + ' столи')
+} else if (num ==53) {
+    console.log(num + ' столи')
+} else if (num ==54) {
+    console.log(num + ' столи')
+} else if (num ==62) {
+    console.log(num + ' столи')
+} else if (num ==63) {
+    console.log(num + ' столи')
+} else if (num ==64) {
+    console.log(num + ' столи')
+}  else if (num ==72) {
+    console.log(num + ' столи')
+} else if (num ==73) {
+    console.log(num + ' столи')
+} else if (num ==74) {
+    console.log(num + ' столи')
+}  else if (num ==82) {
+    console.log(num + ' столи')
+} else if (num ==83) {
+    console.log(num + ' столи')
+} else if (num ==84) {
+    console.log(num + ' столи')
+}  else if (num ==92) {
+    console.log(num + ' столи')
+} else if (num ==93) {
+    console.log(num + ' столи')
+} else if (num ==94) {
+    console.log(num + ' столи')
+}  
+
+ else if (num !== 1 && num !== 2 && num !== 4 
+    && num !== 21 && num !== 31 && num !== 41 && num !== 51 
+    && num !== 61 && num !== 71 && num !== 81 && num !== 91) {
+    console.log(num + ' столів')
+}
