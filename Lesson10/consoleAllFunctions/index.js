53f39586d3321999b6c4ca1276d126330147ff7e
makskrevskyi <91610843+makskrevskyi@users.noreply.github.com>
2021-12-02 21:27:12 +0200

    Add files via upload

---
+++ Lesson10/consoleAllFunctions/index.js
@@ -0,0 +1,15 @@
+let userInfo = {
+    age: 21,
+    height: 185,
+    skinColor: 'white',
+    city: 'Lviv',
+}
+console.log (userInfo);
+console.count (userInfo);
+console.dir (userInfo);
+console.dirxml (userInfo);
+console.error (userInfo);
+console.group (userInfo);
+console.table (userInfo);
+console.trace (userInfo);
+console.warn (userInfo);
